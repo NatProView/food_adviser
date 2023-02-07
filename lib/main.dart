e073729782a100ac59e7fbf7ed80dc5413893f9b
Mateusz Śródka <mateusz.srodka@gmail.com>
2023-02-07 23:36:57 +0100

    Flutter tutorial 6 - Colors & Font

--- lib/main.dart
+++ lib/main.dart
@@ -10,13 +10,24 @@ void main() {
     // Scaffold Widget = takes many different things about base layout of app
     home: Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.red,
         title: const Text("Title"),
         centerTitle: true,
       ),
           body: const Center(
-            child: Text("Siema!"),
+            child: Text(
+                "Siema!",
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2.0,
+                color: Colors.grey,
+                fontFamily: 'BebasNeue',
+              )
+            ),
           ),
       floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.red,
         onPressed: () {  },
         child: const Text("click"),
       ),
