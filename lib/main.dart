63d9f2c163819b986c32f87d1b56a9cd56b50193
Mateusz Śródka <mateusz.srodka@gmail.com>
2023-02-07 18:53:54 +0100

    Flutter tutorial 5 - Scaffold & AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
 
+// czytnij sobie: https://api.flutter.dev/flutter/material/Scaffold-class.html
+
 // entrypoint
 void main() {
   //passujemy tutaj widget, ktory jest root widgetem naszej apki
-  runApp(const MaterialApp(
-    home: Text("Siema!"), //always comma separated
-
-
-
-
+  runApp(MaterialApp(
+    // home: Text("Siema!"), //always comma separated
+    // Scaffold Widget = takes many different things about base layout of app
+    home: Scaffold(
+      appBar: AppBar(
+        title: const Text("Title"),
+        centerTitle: true,
+      ),
+          body: const Center(
+            child: Text("Siema!"),
+          ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {  },
+        child: const Text("click"),
+      ),
+    ),
   ));
 }
