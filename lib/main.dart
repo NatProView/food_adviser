db35a8b675b7657f770da7c3dd7afee3331e0a76
Mateusz Śródka <mateusz.srodka@gmail.com>
2023-02-07 23:50:32 +0100

    Flutter tutorial 7 - Stateless Widgets & Hot Reload

--- lib/main.dart
+++ lib/main.dart
@@ -5,32 +5,45 @@ import 'package:flutter/material.dart';
 // entrypoint
 void main() {
   //passujemy tutaj widget, ktory jest root widgetem naszej apki
-  runApp(MaterialApp(
-    // home: Text("Siema!"), //always comma separated
-    // Scaffold Widget = takes many different things about base layout of app
-    home: Scaffold(
+  runApp(const MaterialApp(
+    home: Home(),
+  ));
+}
+
+// our own widget class by extending StatelessWidget
+// stateless - the state of the widget cannot change
+// over time (as we use the app)
+//
+// stateful - the state of the widget can change over time
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+  //this function overrides basic build function,
+  // not the one we inherited from StatelessWidget
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.red,
         title: const Text("Title"),
         centerTitle: true,
       ),
-          body: const Center(
-            child: Text(
-                "Siema!",
-              style: TextStyle(
-                fontSize: 20.0,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 2.0,
-                color: Colors.grey,
-                fontFamily: 'BebasNeue',
-              )
-            ),
-          ),
+      body: const Center(
+        child: Text(
+            "Siema!",
+            style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.0,
+              color: Colors.grey,
+              fontFamily: 'BebasNeue',
+            )
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.red,
         onPressed: () {  },
         child: const Text("click"),
       ),
-    ),
-  ));
+    );
+  }
 }
